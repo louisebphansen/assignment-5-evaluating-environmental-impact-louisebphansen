943354ec92745e062456591ccd29c922ddd124f7
louisepbhansen <louise.bp@hotmail.com>
2024-05-27 09:05:40 +0200

    updated run script

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
+# activate virtual environment
 source ./env/bin/activate
 
-# ADD UNZIP PART
+# unzip to create emissions folder
+unzip emissions.zip
 
+# run code with default argument
 python3 src/visualize.py
 
+# deactivate virtual environment
 deactivate
